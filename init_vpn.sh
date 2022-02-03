b440ad1d020788740645988147c9bf03d6544e72
Henry Wang <wanghenry1998@gmail.com>
2022-02-03 16:08:50 -0600

    Initialization message

--- init_vpn.sh
+++ init_vpn.sh
@@ -5,6 +5,7 @@ set -e
 mkdir -p .certs
 cat > .certs/script <<EOF
 #!/bin/bash
+set -e
 ovpn init ca
 ovpn init server
 EOF
@@ -12,5 +13,7 @@ EOF
 chmod +x .certs/script
 
 sudo docker-compose run --rm manage_cert /mnt/certs/script
+
+echo "*** initialized completed ***"
 
 rm .certs/script
